47265e74c39d0031bebbb34437b9dda0cf909d58
Dominick Albacore <doalbaco@student.21-school.ru>
2021-11-24 18:48:01 +0300

    huffman

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -3,7 +3,7 @@
 int main(void)
 {
 	std::ifstream input("input.txt");
-	std::ofstream output("output.txt");
+	std::ofstream output("output.txt", std::ios::binary);
 	std::stringstream buffer;
 	PrefixTREE	tree;
 
@@ -11,8 +11,19 @@ int main(void)
 	std::string str = buffer.str();
 
 	std::string	encoded = huffman_encode(str, &tree);
+	std::cout << encoded << std::endl;
+
+	int c;
+
+	for (int i = 0; i < (int) encoded.length() / 8; i++)
+	{
+		c = 0;
+		for (int j = 0; j < 8; j++)
+			c += (encoded[i * 8 + j] == '1') << (7 - j);
+		output.write((char *) &c, 8);
+	}
+
 	std::string	decoded = huffman_decode(encoded, &tree);
 
-	output << decoded;
 	// std::cout << str << std::endl << decoded << std::endl;
 }
