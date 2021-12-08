2b43dd99d87486e14861f04be8a6328255fb96a3
dannromm <danilka.2002.lol@gmail.com>
2021-12-08 21:40:08 +0300

    ex07 done

--- ex07/main.cpp
+++ ex07/main.cpp
@@ -1,37 +1,57 @@
 #include "main.hpp"
 
-std::vector<int>	input()
+t_coins	input_coins()
 {
-	int					n;
-	int					coin;
-	std::vector<int>	coins;
+	int		n;
+	int		coin;
+	t_coins	coins;
 
+	std::cout << "Введите требуемую сумму: ";
+	std::cin >> coins.sum;
 	std::cout << "Введите кол-во номиналов монет: ";
 	std::cin >> n;
-	std::cout << "Введите номиналы (" << n << ") монет:\n";
-	for (int i = 0; i < n; i++)
+	std::cout << "Введите номиналы монет a2...a" << n << ": ";
+	coins.arr.push_back(1);
+	for (int i = 0; i < n - 1; i++)
 	{
 		std::cin >> coin;
-		coins.push_back(coin);
+		coins.arr.push_back(coin);
 	}
 	return (coins);
 }
 
-std::map<int, int>	min_coins(std::vector<int> coins, )
+std::map<int, int>	min_coins(t_coins coins)
 {
 	std::map<int, int>	count;
+	int					curr_sum = coins.sum;
+	int					n;
 
-	std::sort(coins.rbegin(), coins.rend());
+	std::sort(coins.arr.rbegin(), coins.arr.rend());
 
-	for (auto it : coins)
+	for (auto it : coins.arr)
 	{
-
+		n = curr_sum / it;
+		count[it] = n;
+		curr_sum -= n * it;
 	}
 
 	return (count);
 }
 
+void	format_print_ans(std::map<int, int> ans)
+{
+	std::cout << "Необходимо взять:\n";
+	for (auto it = ans.rbegin(); it != ans.rend(); it++)
+	{
+		if (it->second != 0)
+			std::cout << std::setw(5) << it->second << " монет(ы) номиналом " << it->first << "\n";
+	}
+}
+
 int main(void)
 {
-	min_coins(input());
+	std::map<int, int> ans;
+
+	ans = min_coins(input_coins());
+	format_print_ans(ans);
 }
--- ex07/main.hpp
+++ ex07/main.hpp
@@ -2,8 +2,14 @@
 # define MAIN_HPP
 
 # include <iostream>
+# include <iomanip>
 # include <vector>
 # include <map>
+
+using t_coins = struct s_coins {
+	int					sum;
+	std::vector<int>	arr;
+};
 
 # include "debug.hpp"
 
